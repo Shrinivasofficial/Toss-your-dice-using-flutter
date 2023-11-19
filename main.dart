a8ba7d23d56c2ca378d100e3fdf6f44171867d65
Shrinivasofficial <118543369+Shrinivasofficial@users.noreply.github.com>
2023-11-19 17:17:14 +0530

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text("Dice App"),
+      ),
+      body: DicePage(),
+    ),
+  ));
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 1;
+  int rightDiceNumber = 1;
+  void rotateDice(){
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                rotateDice();
+              },
+              child: Image.asset('lib/images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                rotateDice();
+              },
+              child: Image.asset('lib/images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
